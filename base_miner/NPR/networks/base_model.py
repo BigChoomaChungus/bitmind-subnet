3c4da083213eb83124feb6a5f16010a96403ab08
BigChoomaChungus <159674192+BigChoomaChungus@users.noreply.github.com>
2024-12-07 22:46:03 -0600

    Update base_model.py

--- base_miner/NPR/networks/base_model.py
+++ base_miner/NPR/networks/base_model.py
@@ -33,18 +33,18 @@ class BaseModel(nn.Module):
 
     # load models from the disk
     def load_networks(self, epoch):
-        try:
-            self.model.load_state_dict(state_dict['model'])
-        except KeyError as e:
-            self.model.load_state_dict(state_dict)
-
-
         
-        #if self.continue_from_checkpoint:
-        #    load_path = self.continue_from_checkpoint
-        #else:
-        #    load_filename = 'model_epoch_%s.pth' % epoch
-        #    load_path = os.path.join(self.save_dir, load_filename)
+        if self.continue_from_checkpoint:
+            load_path = self.continue_from_checkpoint
+
+            try:
+                self.model.load_state_dict(state_dict['model'])
+            except KeyError as e:
+                self.model.load_state_dict(state_dict)
+        
+        else:
+            load_filename = 'model_epoch_%s.pth' % epoch
+            load_path = os.path.join(self.save_dir, load_filename)
 
         print('loading the model from %s' % load_path)
         # if you are using PyTorch newer than 0.4 (e.g., built from
