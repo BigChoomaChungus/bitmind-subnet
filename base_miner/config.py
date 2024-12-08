d2992d0059f887e85278e94f8fe9ffe4583c154e
BigChoomaChungus <159674192+BigChoomaChungus@users.noreply.github.com>
2024-12-07 20:40:10 -0600

    Update config.py to take out all paths

--- base_miner/config.py
+++ base_miner/config.py
@@ -8,14 +8,14 @@ IMAGE_DATASETS = {
     "real": [
         #{"path": "bitmind/bm-real"},
         #{"path": "bitmind/open-image-v7-256"}, open-image-v7-256
-        {"path": "bitmind/celeb-a-hq"},
+        #{"path": "bitmind/celeb-a-hq"},
         #{"path": "/home/user/.cache/huggingface/bitmind___celeb-a-hq/default/0.0.0/cec732e87335bc65872d90a706e865032daf80fc"},
         #{"path": "bitmind/ffhq-256"},
         #{"path": "bitmind/MS-COCO-unique-256"}
     ],
     "fake": [
         #{"path": "bitmind/bm-realvisxl"},
-        {"path": "bitmind/bm-mobius-10-17-24"},
+        #{"path": "bitmind/bm-mobius-10-17-24"},
         #{"path": "/home/user/.cache/huggingface/bitmind___bm-mobius-10-17-24/default/0.0.0/dc8eb1065fb36e48a03e9cff6824cde003bcece5"},
         #{"path": "bitmind/bm-sdxl"}
     ]
